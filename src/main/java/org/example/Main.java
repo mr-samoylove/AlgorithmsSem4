b66090cea9c2c31bddc23fa123d96be026cef1ab
psina <mr.samoylove@gmail.com>
2023-03-16 11:54:37 +0300

    Algorithms Sem 4 done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ public class Main
     public static void main(String[] args) {
         BinaryTree<Integer> bt = new BinaryTree<>();
 
-        for (int i = 0; i < 1024; i++) {
+        for (int i = 1; i <= 1024; i++) {
             bt.add(i);
         }
 
